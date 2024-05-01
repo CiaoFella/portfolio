33a848e683463a8fb9b4fd65993b45c5feb38616
net11FellaJ <julian.fella@neteleven.de>
2024-05-01 16:04:56 +0200

    cleanup main.js

--- src/main.js
+++ src/main.js
@@ -1,53 +1,13 @@
-let $ = window.$
 import beforeAfterSlider from './features/detailPage/beforeAfterSlider'
 import animateMenu from './features/general/animateMenu'
-import button from './features/general/button'
 import './styles/style.scss'
 import initBarba from './utils/pageTransition'
 import lenis from './utils/smoothScroll'
-import * as UnicornStudio from './utils/unicornStudio/unicornStudio.umd'
-// import * as UnicornStudio from './utils/unicornStudio/unicornStudio.umd'
-import initThreeJs from './utils/initThree'
-import hoverScene from './utils/Scene'
-import setupFlipScroll from './features/homePage/setupFlipScroll'
+import domLoaded from 'dom-loaded'
 
-if (document.readyState !== 'loading') {
-  console.log('document is already ready, just execute code here')
-  setupFlipScroll()
-} else {
-  document.addEventListener('DOMContentLoaded', function () {
-    console.log('document was not ready, place code here')
-    setupFlipScroll()
-  })
-}
-
-let json
-fetch('https://julianfella.netlify.app/unicornStudio/imageHover.json', {
-  cache: 'no-cache',
-}).then((response) => (json = response))
-
-// new hoverScene({
-//   container: document.querySelector('.canvas-wrap'),
-// })
-UnicornStudio.addScene({
-  elementId: 'us-hover', // id of the HTML element to render your scene in (the scene will use its dimensions)
-  fps: 120, // frames per second (0-120) [optional]
-  scale: 1, // rendering scale, use smaller values for performance boost (0.25-1) [optional]
-  dpi: 1, // pixel ratio [optional]
-  projectId: 'myNdurW6zb8lw4aVaq0f', // the id string for your embed (get this from "embed" export)
-  filePath: json, // if youre hosting your own exported json code, point to it here
+await domLoaded.then(() => {
+  initBarba.initBarba()
+  animateMenu()
+  beforeAfterSlider()
+  lenis.start()
 })
-  .then((scene) => {
-    // Scene is ready
-    // To remove a scene, you can use:
-    // scene.destroy()
-  })
-  .catch((err) => {
-    console.error(err)
-  })
-
-// initBarba.initBarba()
-animateMenu()
-beforeAfterSlider()
-button()
-lenis.start()
