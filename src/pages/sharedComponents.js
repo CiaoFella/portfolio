482d1234574d577a0db02c71481a9768edde207b
net11FellaJ <julian.fella@neteleven.de>
2024-05-01 16:45:39 +0200

    clean up shared components

--- src/pages/sharedComponents.js
+++ src/pages/sharedComponents.js
@@ -1,8 +1,9 @@
-import aniamteIcons from '../features/general/aniamteIcons'
 import animateContact from '../features/general/animateContact'
 import animateSectionScale from '../features/general/animateSectionScale'
 import button from '../features/general/button'
+import animateIcons from '../features/general/animateIcons'
+import handleVideos from '../features/general/handleVideos'
 
 export default function sharedComponents() {
-  return button(), animateSectionScale(), animateContact(), aniamteIcons()
+  return button(), animateSectionScale(), animateContact(), animateIcons()
 }
