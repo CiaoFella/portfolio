40a9e4294b589c1bf3effd35d0da8496ae1381df
net11FellaJ <julian.fella@neteleven.de>
2024-05-21 14:13:42 +0200

    adapt about page

--- src/pages/aboutPage.js
+++ src/pages/aboutPage.js
@@ -1,8 +1,27 @@
 import myPhilosophy from '../features/aboutPage/myPhilosophy'
 import myProcess from '../features/aboutPage/myProcess'
-import animateHero from '../features/general/animateHero'
+import animateCardList from '../features/general/animateCardList'
+import animateConnectScroll from '../features/general/animateConnectScroll'
+import animateFooter from '../features/general/animateFooter'
+import animateHeadlineScroll from '../features/general/animateHeadlineScroll'
+import animateImages from '../features/general/animateImages'
+import animatePassions from '../features/general/animatePassion'
+import animateScrollCounter from '../features/general/animateScrollCounter'
+import animateTextContent from '../features/general/animateTextContent'
 import sharedComponents from './sharedComponents'
 
 export default function aboutPage() {
-  return [myProcess(), myPhilosophy(), sharedComponents()]
+  return [
+    animateTextContent(),
+    myProcess(),
+    myPhilosophy(),
+    sharedComponents(),
+    animateCardList(),
+    animatePassions(),
+    animateImages(),
+    animateScrollCounter(),
+    animateHeadlineScroll(),
+    animateConnectScroll(),
+    animateFooter(),
+  ]
 }
