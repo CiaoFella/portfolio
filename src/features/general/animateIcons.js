9924dbc9f57bedd99e5a04eae80a9900c29ad2ad
net11FellaJ <julian.fella@neteleven.de>
2024-05-21 14:13:17 +0200

    add defaults to icon animation

--- src/features/general/animateIcons.js
+++ src/features/general/animateIcons.js
@@ -6,16 +6,16 @@ export default function animateIcons() {
   const allIcons = $('[data-animate-icon]')
   allIcons.each((index, icon) => {
     const iconName = $(icon).data('animateIcon')
-    const iconTl = gsap.timeline({ paused: true })
+    const iconTl = gsap.timeline({ paused: true, defaults: { duration: 0.75 } })
     switch (iconName) {
       case 'star':
-        iconTl.to(icon, { scale: 0.5, ease: 'back.inOut', duration: 0.5 })
+        iconTl.to(icon, { scale: 0.5, ease: 'back.inOut' })
         break
       case 'wheel':
-        iconTl.to(icon, { rotateZ: 90, ease: 'back.inOut', duration: 0.5 })
+        iconTl.to(icon, { rotateZ: 90, ease: 'back.inOut' })
         break
       case 'price':
-        iconTl.to(icon, { rotateZ: 180, ease: 'back.inOut', duration: 1 })
+        iconTl.to(icon, { rotateZ: 180, ease: 'back.inOut' })
         break
     }
 
