44638c6e19b62bf7493382af0d62f09cfdf78842
net11FellaJ <julian.fella@neteleven.de>
2024-06-08 20:20:48 +0200

    move closeMenu

--- src/utils/closeMenu.js
+++
@@ -1,9 +0,0 @@
-let $ = window.$
-
-export default function closeMenu() {
-  const navbarWrap = $('.navbar_wrap')
-  const menuTrigger = navbarWrap.find('.js-menu-trigger')
-  if (menuTrigger.hasClass('is-active')) {
-    menuTrigger.click()
-  }
-}
