07f47bbfbf4502d380a739fa798765faa1d0d36c
net11FellaJ <julian.fella@neteleven.de>
2024-06-08 20:21:52 +0200

    adapt barba setup

--- src/utils/setupBarba.js
+++ src/utils/setupBarba.js
@@ -4,16 +4,25 @@ import barba from '@barba/core'
 import gsap from 'gsap'
 import ScrollTrigger from 'gsap/dist/ScrollTrigger'
 
-import closeMenu from './closeMenu'
+
 import { isDesktop } from './variables'
-import aboutPage from '../pages/aboutPage'
-import homePage from '../pages/homePage'
-import listPage from '../pages/listPage'
-import callOnceComponents from '../pages/callOnceComponents'
 import { cursor, magneticCursor } from './customCursor/index'
 import transitions from './animatePageTransitions'
 import animateTransitions from './animatePageTransitions'
-import awardsPage from '../pages/awardsPage'
+import animateHero from '../features/general/animateHero'
+import helperFunctions from './helperFunctions'
+import { proxy } from './pageReadyHandler'
+
+import initDetailPage from '../pages/initPages/initDetailPage'
+import killDetailPage from '../pages/killPages/killDetailPage'
+import initHomePage from '../pages/initPages/initHomePage'
+import initAboutPage from '../pages/initPages/initAboutPage'
+import killAboutPage from '../pages/killPages/killAboutPage'
+import initAwardPage from '../pages/initPages/initAwardPage'
+import killAwardPage from '../pages/killPages/killAwardPage'
+import initListPage from '../pages/initPages/listPage'
+import killListPage from '../pages/killPages/killListPage'
+import { closeMenu } from '../features/general/menu'
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -33,7 +42,7 @@ function resetGSAP() {
   let existingScrollTriggers = ScrollTrigger.getAll()
   for (let index = 0; index < existingScrollTriggers.length; index++) {
     const singleTrigger = existingScrollTriggers[index]
-    singleTrigger.kill(false)
+    singleTrigger.kill()
   }
   ScrollTrigger.refresh()
   window.dispatchEvent(new Event('resize'))
@@ -44,7 +53,6 @@ function setupBarba() {
 
   barba.hooks.after((data) => {
     currentPage = $('[data-barba-namespace]').data('barbaNamespace')
-    initPage(currentPage)
     resetWebflow(data)
     matchMedia.add(isDesktop, () => {
       const $customCrusor = $('.cb-cursor')
@@ -55,33 +63,146 @@ function setupBarba() {
   })
   barba.hooks.beforeLeave(() => {
     closeMenu()
-    killPage(currentPage)
+  })
+  barba.hooks.afterEnter(() => {
+    window.scrollTo({ top: 0, behavior: 'auto' })
+  })
+
+  barba.hooks.once(() => {
+    document.addEventListener('onPageReady', (event) => {
+      if (event.detail === true) {
+        animateHero().play()
+        if (currentPage === 'detail-page') {
+          const navBar = $('[data-animate=nav-bar]')
+          const detailNav = $('[data-animate=detail-nav-wrap]')
+          helperFunctions.slideInNavigations(navBar, detailNav, 1).play()
+        }
+      } else if (event.detail === false) {
+      }
+    })
   })
 
   barba.init({
     preventRunning: true,
-    views: [{}],
+    views: [
+      {
+        namespace: 'home-page',
+        beforeEnter() {
+          initHomePage()
+        },
+        beforeLeave() {
+          killDetailPage()
+        },
+      },
+      {
+        namespace: 'about-page',
+        beforeEnter() {
+          initAboutPage()
+        },
+        beforeLeave() {
+          killAboutPage()
+        },
+      },
+      {
+        namespace: 'list-page',
+        beforeEnter() {
+          initListPage()
+        },
+        beforeLeave() {
+          killListPage()
+        },
+      },
+      {
+        namespace: 'award-page',
+        beforeEnter() {
+          initAwardPage()
+        },
+        beforeLeave() {
+          killAwardPage()
+        },
+      },
+      {
+        namespace: 'detail-page',
+        afterEnter() {
+          requestAnimationFrame(() => {
+            initDetailPage()
+            helperFunctions.refreshScrollTriggers()
+          })
+        },
+        beforeLeave() {
+          ScrollTrigger.getAll().forEach((trigger) => trigger.kill())
+          killDetailPage()
+        },
+      },
+    ],
     transitions: [
       {
+        to: {
+          namespace: ['about-page', 'home-page', 'list-page', 'award-page'],
+        },
         async leave(data) {
           await transitions.transitionIn()
           $(data.current).hide()
-          resetGSAP()
+          // resetGSAP()
         },
         enter() {
-          transitions.transitionOut()
-          $(window).scrollTop(0)
+          transitions.transitionOut(false)
         },
       },
       {
         once: () => {
-          // animateTransitions.loader(5)
-          currentPage = $('[data-barba-namespace]').data('barbaNamespace')
-          callOnceComponents()
-          initPage(currentPage)
+          animateTransitions.loader(2)
           matchMedia.add(isDesktop, () => {
             cursor.init()
             magneticCursor()
+          })
+        },
+      },
+      {
+        name: 'list-detail-transition',
+        to: {
+          namespace: ['detail-page'],
+        },
+        from: {
+          namespace: ['list-page'],
+        },
+        async enter(data) {
+          helperFunctions.createHeroSplitTypes($('[data-hero-element=headline]'))
+          animateTransitions.makeItemActive(data)
+          const detailTransitionTl = gsap.timeline()
+          return detailTransitionTl.to(data.current.container, { opacity: 0, duration: 0.5 }).call(
+            async () => {
+              $(data.next.container).addClass('fixed')
+              await animateTransitions.flipAnimation(
+                $('.active-flip-item'),
+                $('[data-flip-element=hero]'),
+                $('[data-flip-element=first-target]')
+              )
+            },
+            [],
+            '<'
+          )
+        },
+        after(data) {
+          $(data.next.container).removeClass('fixed')
+          $('.active-flip-item').removeClass('active-flip-item')
+        },
+      },
+      {
+        name: 'detail-detail-transition',
+        to: {
+          namespace: ['detail-page'],
+        },
+        from: {
+          namespace: ['detail-page'],
+        },
+        enter() {
+          proxy.pageReady = false
+        },
+        after() {
+          requestAnimationFrame(() => {
+            proxy.pageReady = true
+            window.scrollTo({ top: 0, behavior: 'auto' })
           })
         },
       },
@@ -89,43 +210,4 @@ function setupBarba() {
   })
 }
 
-function killPage(currentPage) {
-  if (currentPage) {
-    if (currentPage === 'home-page') {
-      homePage().forEach((anim) => {
-        anim.forEach((trigger) => {
-          trigger.kill()
-        })
-      })
-    } else if (currentPage === 'about-page') {
-      aboutPage().forEach((anim) => {
-        anim.forEach((trigger) => {
-          trigger.kill()
-        })
-      })
-    }
-  } else {
-    return
-  }
-}
-
-function initPage(currentPage) {
-  if (currentPage) {
-    if (currentPage === 'home-page') {
-      homePage()
-    } else if (currentPage === 'list-page') {
-      listPage()
-    } else if (currentPage === 'detail-page') {
-      return
-    } else if (currentPage === 'about-page') {
-      aboutPage()
-    } else if (currentPage === 'awards-page') {
-      awardsPage()
-    } else if (currentPage === 'overview-page') {
-      return
-    }
-  } else {
-    return
-  }
-}
-export default { setupBarba }
+export default setupBarba
