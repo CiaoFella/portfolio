60b2f5aa8d9661d65e1bc20a6a70271097c6951a
net11FellaJ <julian.fella@neteleven.de>
2024-06-23 01:02:43 +0200

    adapt path animation

--- src/features/aboutPage/myPhilosophy.js
+++ src/features/aboutPage/myPhilosophy.js
@@ -1,29 +1,51 @@
 import gsap from 'gsap'
 import ScrollTrigger from 'gsap/dist/ScrollTrigger'
+import { isLandscape, isMobile } from '../../utils/variables'
 
 gsap.registerPlugin(ScrollTrigger)
 
 let ctx
+const mm = gsap.matchMedia()
 
 export default function initMyPhilosophy() {
   ctx = gsap.context(() => {
     const myPhilosophyWrap = $('[data-animate=my-philosophy-wrap]')
     const path = myPhilosophyWrap.find('[data-animate=philosophy-line-path]')
+    const mobilePath = myPhilosophyWrap.find('[data-animate=philosophy-line-path-mobile]')
 
-    const length = path[0].getTotalLength()
-    path.css('stroke-dasharray', length)
-    path.css('stroke-dashoffset', length)
+    let currentPath = path
+
+    mm.add(isMobile, () => {
+      currentPath = mobilePath
+    })
+
+    const length = currentPath[0].getTotalLength()
+    currentPath.css('stroke-dasharray', length)
+    currentPath.css('stroke-dashoffset', length)
 
     const myPhilosophyTl = gsap.timeline()
 
-    myPhilosophyTl.to(path, { strokeDashoffset: 0, duration: 1 })
+    myPhilosophyTl.to(currentPath, { strokeDashoffset: 0, duration: 1 })
 
-    ScrollTrigger.create({
-      animation: myPhilosophyTl,
-      trigger: myPhilosophyWrap,
-      start: '2.5% top',
-      end: 'bottom bottom',
-      scrub: true,
+    mm.add(isLandscape, () => {
+      ScrollTrigger.create({
+        animation: myPhilosophyTl,
+        trigger: myPhilosophyWrap,
+        start: '2.5% top',
+        end: 'bottom bottom',
+        scrub: true,
+      })
+    })
+
+    mm.add(isMobile, () => {
+      ScrollTrigger.create({
+        animation: myPhilosophyTl,
+        trigger: myPhilosophyWrap,
+        start: '15% center',
+        end: 'bottom center',
+        scrub: true,
+        markers: true,
+      })
     })
   })
 }
