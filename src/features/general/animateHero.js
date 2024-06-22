3eac743c86d662b8572897dec501f6ec48fda850
net11FellaJ <julian.fella@neteleven.de>
2024-06-22 15:37:19 +0200

    add home hero

--- src/features/general/animateHero.js
+++ src/features/general/animateHero.js
@@ -5,7 +5,13 @@ import letterSwitch from './animateLetterSwitch'
 import gsap from 'gsap'
 import ScrollTrigger from 'gsap/dist/ScrollTrigger'
 import SplitType from 'split-type'
-import { bottomClipPath, fullClipPath, topClipPath } from '../../utils/variables'
+import {
+  bottomClipPath,
+  centerHorizontalClipPath,
+  centerVerticalClipPath,
+  fullClipPath,
+  topClipPath,
+} from '../../utils/variables'
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -13,11 +19,20 @@ export default function animateHero() {
   const sectionHero = $('[data-hero]')
   const heroType = sectionHero.data('hero')
   const heroHeadline = sectionHero.find('[data-hero-element=headline]')
-  const heroVideoWrap = sectionHero.find('[data-animate=hero-video-target-wrap]')
-  const heroVideo = sectionHero.find('[data-animate=hero-video-target]')
+
+  const heroVideoOuter = sectionHero.find('[data-hero-element=video-outer]')
+  const heroVideoWrap = sectionHero.find('[data-hero-element=video-wrap]')
+  const heroVideo = sectionHero.find('[data-hero-element=video]')
+  const heroCta = sectionHero.find('[data-hero-element=cta]')
+  const heroScrollIndiator = sectionHero.find('[data-hero-element=scroll]')
+
   const heroText = sectionHero.find('[data-animate=letter-switch]')
+  const heroParagraph = sectionHero.find('[data-hero-element=paragraph]')
+  const heroTeaser = sectionHero.find('[data-hero-element=teaser-wrap]')
+  const heroTeaserImg = sectionHero.find('[data-hero-element=teaser-img]')
   const heroItemRight = $(sectionHero).find('[data-animate=hero-item-move][data-move=right]')
   const heroItemLeft = $(sectionHero).find('[data-animate=hero-item-move][data-move=left]')
+
   const heroDetailWrap = $(sectionHero).find('[data-animate=hero-detail-wrap]')
   const heroLocationWrap = $(sectionHero).find('[data-animate=hero-location-wrap]')
   const heroDetailText = heroDetailWrap.find('p')
@@ -33,6 +48,28 @@ export default function animateHero() {
     case 'text':
       const headlineTextSplit = new SplitType(heroHeadline, { types: 'chars' })
       heroTl.to([headlineTextSplit.chars, heroHeadline], { y: 0, duration: 1.5, delay: 0.2, stagger: 0.03 }, 0)
+      break
+    case 'home':
+      const headlineHomeSplit = new SplitType(heroHeadline, { types: 'chars' })
+      const paragraphSplit = new SplitType(heroParagraph, { types: 'lines' })
+      heroTl
+        .to([headlineHomeSplit.chars, heroHeadline], { y: 0, duration: 1, delay: 0.2, stagger: 0.03 }, 0)
+        .fromTo(
+          heroTeaser,
+          { clipPath: centerVerticalClipPath },
+          { clipPath: fullClipPath, duration: 1.5, stagger: 0.1 },
+          '<+50%'
+        )
+        .to(heroTeaserImg, { scale: 1.05, stagger: 0.1, duration: 2, ease: 'power2.out' }, '<+10%')
+        .fromTo(
+          paragraphSplit.lines,
+          { clipPath: topClipPath, yPercent: 100 },
+          { clipPath: fullClipPath, yPercent: 0, duration: 1.5, stagger: 0.1 },
+          '<+25%'
+        )
+        .fromTo(heroVideoOuter, { clipPath: bottomClipPath }, { clipPath: fullClipPath }, '>-25%')
+        .fromTo(heroCta, { clipPath: bottomClipPath }, { clipPath: fullClipPath }, '<')
+        .to(heroScrollIndiator, { y: 0 }, '<')
       break
     case 'detail':
       const headlineDetailSplit = new SplitType(heroHeadline, { types: 'chars' })
