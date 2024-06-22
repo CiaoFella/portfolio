a4cac413a1a11ef69667ec9d667ae867137d30b2
net11FellaJ <julian.fella@neteleven.de>
2024-06-22 15:34:28 +0200

    add paragraph to initial state

--- src/utils/createInitialState.js
+++ src/utils/createInitialState.js
@@ -4,5 +4,7 @@ let $ = window.$
 
 export default function createInitialState() {
   const heroHeadlines = $('[data-hero] [data-hero-element=headline]')
+  const heroParagraphs = $('[data-hero] [data-hero-element=paragraph]')
   new SplitType(heroHeadlines, { types: 'chars' })
+  new SplitType(heroParagraphs, { types: 'lines' })
 }
