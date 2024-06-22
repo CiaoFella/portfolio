28ece91f824a2daac8e1179be1408aabf98466c7
net11FellaJ <julian.fella@neteleven.de>
2024-06-22 15:35:14 +0200

    add achievement feature

---
+++ src/features/homePage/achievement.js
@@ -0,0 +1,57 @@
+let $ = window.$
+
+import gsap from 'gsap'
+import ScrollTrigger from 'gsap/dist/ScrollTrigger'
+import { bottomClipPath, fullClipPath, topClipPath } from '../../utils/variables'
+import SplitType from 'split-type'
+
+let ctx
+
+export default function initAchievement() {
+  ctx = gsap.context(() => {
+    const achievementSection = $('[data-animate=achievement-section]')
+    const achievementHeadline = achievementSection.find('[data-animate=achievement-headline]')
+    const achievementElements = achievementSection.find('[data-animate=achievement-element]')
+
+    const headlineSplit = new SplitType(achievementHeadline, { types: 'chars' })
+
+    const achievementTl = gsap.timeline({ defaults: { duration: 1, ease: 'power1.inOut' } })
+
+    achievementTl.fromTo(
+      headlineSplit.chars,
+      { clipPath: topClipPath, y: 100 },
+      { y: 0, clipPath: fullClipPath, duration: 1, stagger: 0.005 },
+      0
+    )
+
+    achievementElements.each((index, element) => {
+      const value = $(element).find('[data-animate=achievement-value]')
+      const type = $(element).find('[data-animate=achievement-type]')
+      const unit = $(element).find('[data-animate=achievement-unit]')
+      const valueSplit = new SplitType(value, { types: 'chars' })
+      const typeSplit = new SplitType(type, { types: 'chars' })
+      const unitSplit = new SplitType(unit, { types: 'chars' })
+      achievementTl
+        .from(valueSplit.chars, { y: 125, duration: 0.5, stagger: 0.01 }, '<+0.25')
+        .from(typeSplit.chars, { y: 125, duration: 0.5, stagger: 0.001 }, '<')
+        .from(unitSplit.chars, { y: 125, duration: 0.5, stagger: 0.001 }, '<')
+        .to(unitSplit.chars, { y: -125, duration: 0.5, stagger: 0.001 }, '>+0.1')
+        .to(valueSplit.chars, { y: -125, duration: 0.5, stagger: 0.01 }, '<')
+        .to(typeSplit.chars, { y: -125, duration: 0.5, stagger: 0.001 }, '<')
+    })
+
+    ScrollTrigger.create({
+      animation: achievementTl,
+      trigger: achievementSection,
+      start: 'top top',
+      end: 'bottom bottom',
+      scrub: 1,
+    })
+  })
+}
+
+export function killAchievement() {
+  if (ctx) {
+    ctx.kill()
+  }
+}
