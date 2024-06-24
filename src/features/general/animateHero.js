114744016de8c95f737b0610a8657910c5c46f0b
net11FellaJ <julian.fella@neteleven.de>
2024-06-24 17:49:52 +0200

    remove unused variables and obsolete video animation code

--- src/features/general/animateHero.js
+++ src/features/general/animateHero.js
@@ -20,24 +20,12 @@ export default function animateHero() {
   const heroType = sectionHero.data('hero')
   const heroHeadline = sectionHero.find('[data-hero-element=headline]')
 
-  const heroVideoOuter = sectionHero.find('[data-hero-element=video-outer]')
-  const heroVideoWrap = sectionHero.find('[data-hero-element=video-wrap]')
-  const heroVideo = sectionHero.find('[data-hero-element=video]')
   const heroCta = sectionHero.find('[data-hero-element=cta]')
   const heroScrollIndiator = sectionHero.find('[data-hero-element=scroll]')
 
-  const heroText = sectionHero.find('[data-animate=letter-switch]')
   const heroParagraph = sectionHero.find('[data-hero-element=paragraph]')
   const heroTeaser = sectionHero.find('[data-hero-element=teaser-wrap]')
   const heroTeaserImg = sectionHero.find('[data-hero-element=teaser-img]')
-  const heroItemRight = $(sectionHero).find('[data-animate=hero-item-move][data-move=right]')
-  const heroItemLeft = $(sectionHero).find('[data-animate=hero-item-move][data-move=left]')
-
-  const heroDetailWrap = $(sectionHero).find('[data-animate=hero-detail-wrap]')
-  const heroLocationWrap = $(sectionHero).find('[data-animate=hero-location-wrap]')
-  const heroDetailText = heroDetailWrap.find('p')
-  const heroDetailButtons = heroDetailWrap.find('.button')
-  const heroLocationText = heroLocationWrap.children()
 
   const heroTl = gsap.timeline({
     paused: true,
@@ -67,9 +55,6 @@ export default function animateHero() {
           { clipPath: fullClipPath, yPercent: 0, duration: 1.5, stagger: 0.1 },
           '<+25%'
         )
-        .fromTo(heroVideoOuter, { clipPath: bottomClipPath }, { clipPath: fullClipPath }, '>-25%')
-        .fromTo(heroCta, { clipPath: bottomClipPath }, { clipPath: fullClipPath }, '<')
-        .to(heroScrollIndiator, { y: 0 }, '<')
       break
     case 'detail':
       const headlineDetailSplit = new SplitType(heroHeadline, { types: 'chars' })
@@ -81,76 +66,6 @@ export default function animateHero() {
           { clipPath: fullClipPath, duration: 1.5 },
           '<'
         )
-      break
-    case 'video':
-      letterSwitch.setLetterSwitch(heroText)
-      heroTl.call(() => letterSwitch.animateLetter(heroText, 1).play(), [], 0)
-
-      heroTl
-        .fromTo(
-          heroVideo,
-          { scale: 1.3 },
-          {
-            scale: 1,
-            duration: 1.5,
-            ease: 'expo.inOut',
-          },
-          0 - 0.5
-        )
-        .from(heroItemRight, { xPercent: -50, duration: 2, ease: 'expo.inOut' }, '>')
-        .from(heroItemLeft, { xPercent: 50, duration: 2, ease: 'expo.inOut' }, '<')
-        .from(
-          heroVideoWrap,
-          {
-            xPercent: -20,
-            duration: 2,
-            ease: 'expo.inOut',
-          },
-          '<'
-        )
-
-      if (heroDetailText.length > 0) {
-        const heroDetailTextSplit = new SplitType(heroDetailText, {
-          types: 'lines',
-        })
-        heroTl.from(
-          heroDetailTextSplit.lines,
-          {
-            clipPath: bottomClipPath,
-            yPercent: 100,
-            duration: 2,
-            stagger: 0.1,
-            ease: 'expo.out',
-          },
-          '>'
-        )
-      }
-
-      if (heroDetailButtons.length > 0) {
-        heroTl.from(
-          heroDetailButtons,
-          {
-            yPercent: 110,
-            duration: 1,
-            stagger: 0.05,
-            ease: 'expo.out',
-          },
-          '<+0.5'
-        )
-      }
-
-      if (heroLocationWrap.length > 0) {
-        heroTl.from(
-          heroLocationText,
-          {
-            yPercent: 110,
-            duration: 1,
-            stagger: 0.05,
-            ease: 'expo.out',
-          },
-          '<'
-        )
-      }
       break
     default:
       break
