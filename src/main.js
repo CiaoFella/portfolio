db859fc89220fcf9915b41844f67fa53fba2fcb3
net11FellaJ <julian.fella@neteleven.de>
2024-06-25 16:53:17 +0200

    add dom loaded event listener

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import beforeAfterSlider from './features/detailPage/beforeAfterSlider'
 import initMenu from './features/general/menu'
 import './styles/style.scss'
 import setupBarba from './utils/setupBarba'
 
-setupBarba()
-initMenu()
-beforeAfterSlider()
+document.addEventListener('DOMContentLoaded', (event) => {
+  setupBarba()
+  initMenu()
+})
