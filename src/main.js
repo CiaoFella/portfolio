5c0ba8ce4910ed551b5bd20c618b0d7642af4895
net11FellaJ <julian.fella@neteleven.de>
2024-06-25 16:56:19 +0200

    remove dom loaded event listener

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import initMenu from './features/general/menu'
 import './styles/style.scss'
 import setupBarba from './utils/setupBarba'
 
-document.addEventListener('DOMContentLoaded', (event) => {
+
   setupBarba()
   initMenu()
-})
+
