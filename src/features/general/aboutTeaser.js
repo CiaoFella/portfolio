d1c73a0846aebb011d02f5d2f17765bbabc35bc4
net11FellaJ <julian.fella@neteleven.de>
2024-06-27 16:59:42 +0200

    remove about Teaser headline

--- src/features/general/aboutTeaser.js
+++ src/features/general/aboutTeaser.js
@@ -12,18 +12,15 @@ export default function initAboutTeaser() {
   ctx = gsap.context(() => {
     const section = $('[data-animate-about=teaser-section]')
     const button = section.find('.button')
-    const headline = section.find('[data-animate=about-teaser-headline]')
     const imgWrap = section.find('[data-animate=about-teaser-img-wrap]')
     const text = section.find('[data-animate=about-teaser-text]')
 
-    const headlineSplit = new SplitType(headline, { types: 'chars' })
     const textSplit = new SplitType(text, { types: 'lines', absolute: true })
 
     const aboutTeaserTl = gsap.timeline({ defaults: { duration: 1, ease: 'power3.out' } })
 
     aboutTeaserTl
       .fromTo(button, { clipPath: topClipPath }, { clipPath: fullClipPath })
-      .from(headlineSplit.chars, { yPercent: 100, stagger: 0.01 }, '<')
       .fromTo(
         textSplit.lines,
         { yPercent: 100, clipPath: bottomClipPath },
