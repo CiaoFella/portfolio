6a2954cfdf603445ad15e7282e028a7df79f9ea3
net11FellaJ <julian.fella@neteleven.de>
2024-06-27 17:00:21 +0200

    add fallback to currentTime

--- src/features/general/currentTime.js
+++ src/features/general/currentTime.js
@@ -5,41 +5,50 @@ import gsap from 'gsap'
 let ctx
 
 export default function initCurrentTime() {
-  const timeElement = document.querySelector('[data-time=element]')
+  ctx = gsap.context(() => {
+    let timeElement
+    timeElement = document.querySelectorAll('[data-time=element]')
 
-  function updateTime() {
-    const now = new Date()
-    const options = {
-      timeZone: 'Europe/Berlin',
-      month: 'long',
-      day: 'numeric',
-      hour: '2-digit',
-      minute: '2-digit',
-      hour12: false,
-      locale: 'en-US',
+    timeElement.length > 1 ? (timeElement = timeElement[timeElement.length - 1]) : (timeElement = timeElement[0])
+
+    function updateTime() {
+      const now = new Date()
+      const options = {
+        timeZone: 'Europe/Berlin',
+        month: 'long',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: false,
+        locale: 'en-US',
+      }
+      const formatter = new Intl.DateTimeFormat('en-US', options)
+      const parts = formatter.formatToParts(now)
+
+      const dateString = `${parts.find((part) => part.type === 'month').value} ${
+        parts.find((part) => part.type === 'day').value
+      }`
+      const hours = parts.find((part) => part.type === 'hour').value
+      const minutes = parts.find((part) => part.type === 'minute').value
+
+      timeElement.innerHTML = `${dateString}, ${hours}<strong id="colon">:</strong>${minutes}`
+
+      if ($('#colon').length > 0) {
+        gsap.to('#colon', {
+          opacity: 0.5,
+          duration: 1,
+          ease: 'none',
+          repeat: -1,
+          yoyo: true,
+        })
+      }
     }
-    const formatter = new Intl.DateTimeFormat('en-US', options)
-    const parts = formatter.formatToParts(now)
 
-    const dateString = `${parts.find((part) => part.type === 'month').value} ${
-      parts.find((part) => part.type === 'day').value
-    }`
-    const hours = parts.find((part) => part.type === 'hour').value
-    const minutes = parts.find((part) => part.type === 'minute').value
-
-    timeElement.innerHTML = `${dateString}, ${hours}<strong id="colon">:</strong>${minutes}`
-
-    gsap.to('#colon', {
-      opacity: 0,
-      duration: 0.5,
-      ease: 'none',
-      repeat: -1,
-      yoyo: true,
-    })
-  }
-
-  updateTime()
-  setInterval(updateTime, 1000)
+    if (timeElement) {
+      updateTime()
+      setInterval(updateTime, 1000)
+    }
+  })
 }
 
 export function killCurrentTime() {
