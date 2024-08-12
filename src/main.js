a4ffe84a666a09f9be186f29893a407eb6a5d015
net11FellaJ <julian.fella@neteleven.de>
2024-08-13 00:40:31 +0200

    remove console.log

--- src/main.js
+++ src/main.js
@@ -28,7 +28,6 @@ function loadPageModule(pageName) {
   import(/* webpackIgnore: true */ `${baseUrl}pages/${pageName}.js`)
     .then((module) => {
       currentAnimationModule = module.default || {}
-      console.log(`${baseUrl}pages/${pageName}.js`)
       if (typeof currentAnimationModule.init === 'function') {
         currentAnimationModule.init()
       } else {
