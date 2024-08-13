0d0bb5a075fbc0e8abd40bb4ff91526ac343301d
net11FellaJ <julian.fella@neteleven.de>
2024-08-13 09:34:11 +0200

    update cursor

--- src/utils/setupBarba.js
+++ src/utils/setupBarba.js
@@ -3,7 +3,7 @@ let $ = window.$
 import { gsap, barba } from '../vendor.js'
 
 import { isDesktop } from './variables.js'
-import { cursor, magneticCursor } from './customCursor/index.js'
+import { cursor, magneticCursor } from './customCursor/export.js'
 import transitions from './animatePageTransitions.js'
 import animateTransitions from './animatePageTransitions.js'
 import animateHero from '../features/general/animateHero.js'
@@ -14,7 +14,6 @@ import { closeMenu } from '../features/general/menu.js'
 import lenis from './smoothScroll.js'
 import { animateContactForm } from '../features/contactPage/contactForm.js'
 import animatePageTransitions from './animatePageTransitions.js'
-import createInitialState from './createInitialState.js'
 
 const matchMedia = gsap.matchMedia()
 
