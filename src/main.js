e3f82279683c81ee017632a5da01468a629890c8
net11FellaJ <julian.fella@neteleven.de>
2024-08-26 15:43:26 +0200

    remove log

--- src/main.js
+++ src/main.js
@@ -14,7 +14,6 @@ function resetWebflow(data) {
   let webflowPageId = $(dom).find('html').attr('data-wf-page')
   $('html').attr('data-wf-page', webflowPageId)
   window.Webflow && window.Webflow.destroy()
-  console.log(window.Webflow)
   window.Webflow && window.Webflow.window.Webflow && window.Webflow.ready()
   window.Webflow && window.Webflow.require('ix2').init()
   window.Webflow && window.Webflow.require('lottie').init()
